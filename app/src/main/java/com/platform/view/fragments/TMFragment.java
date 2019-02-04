fbabe4246f14b0dd6fd339ddfd50e6465e9d71b8
AnkitaKinj <ankita_kinj@yahoo.co.in>
2019-02-04 10:30:23 +0530

    Room initialization

--- app/src/main/java/com/platform/view/fragments/TMFragment.java
+++ app/src/main/java/com/platform/view/fragments/TMFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -74,6 +75,7 @@ public class TMFragment extends Fragment implements View.OnClickListener, TMTask
     public void showPendingRequests(List<PendingRequest> pendingRequestList) {
         if (pendingRequestList != null && !pendingRequestList.isEmpty()) {
             NewTMAdapter newTMAdapter = new NewTMAdapter(pendingRequestList);
+            rvPendingRequests.setLayoutManager(new LinearLayoutManager(getActivity()));
             rvPendingRequests.setAdapter(newTMAdapter);
         }
     }
