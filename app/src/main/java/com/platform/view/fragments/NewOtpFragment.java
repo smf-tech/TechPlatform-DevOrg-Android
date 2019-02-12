ddcd4ab674acc5e0cb546e4f4a29f8d9af49ee88
Koustubh Karmalkar <koustubh_karmalkar@persistent.com>
2019-02-12 16:56:00 +0530

    Renamed files

--- app/src/main/java/com/platform/view/fragments/NewOtpFragment.java
+++ app/src/main/java/com/platform/view/fragments/NewOtpFragment.java
@@ -84,7 +84,7 @@ public class NewOtpFragment extends Fragment implements View.OnClickListener, Pl
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.otp_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_otp, container, false);
     }
 
     @Override
