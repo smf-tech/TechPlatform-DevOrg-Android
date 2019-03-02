82f019b7dcdea11e5a48f97768a33a532198757c
AnkitaKinj <ankita_kinj@yahoo.co.in>
2019-03-02 16:31:37 +0530

    Drop down dependency issue solved.

---
+++ app/src/main/java/com/platform/widgets/PlatformSpinner.java
@@ -0,0 +1,42 @@
+package com.platform.widgets;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatSpinner;
+import android.util.AttributeSet;
+
+public class PlatformSpinner extends AppCompatSpinner {
+    public PlatformSpinner(Context context) {
+        super(context);
+    }
+
+    public PlatformSpinner(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PlatformSpinner(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void
+    setSelection(int position, boolean animate) {
+        boolean sameSelected = position == getSelectedItemPosition();
+        super.setSelection(position, animate);
+        if (sameSelected) {
+            // Spinner does not call the OnItemSelectedListener if the same item is selected, so do it manually now
+            getOnItemSelectedListener().onItemSelected(this, getSelectedView(), position, getSelectedItemId());
+        }
+    }
+
+    @Override
+    public void
+    setSelection(int position) {
+        boolean sameSelected = position == getSelectedItemPosition();
+        super.setSelection(position);
+        if (sameSelected) {
+            // Spinner does not call the OnItemSelectedListener if the same item is selected, so do it manually now
+            getOnItemSelectedListener().onItemSelected(this, getSelectedView(), position, getSelectedItemId());
+        }
+    }
+
+}
