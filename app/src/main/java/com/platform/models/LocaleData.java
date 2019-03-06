e39fae458b14d030733e01a3614024a1d6d90b10
Koustubh Karmalkar <koustubh_karmalkar@persistent.com>
2019-03-06 14:34:52 +0530

    Fixed lint issues

--- app/src/main/java/com/platform/models/LocaleData.java
+++ app/src/main/java/com/platform/models/LocaleData.java
@@ -9,7 +9,7 @@ public class LocaleData {
 
     @SerializedName("default")
     @Expose
-    private String _default;
+    private String en;
     @SerializedName("mr")
     @Expose
     private String mr;
@@ -17,28 +17,16 @@ public class LocaleData {
     @Expose
     private String hi;
 
-    public String get_default() {
-        return _default;
+    String getEn() {
+        return en;
     }
 
-    public void set_default(String _default) {
-        this._default = _default;
-    }
-
-    public String getMr() {
+    String getMr() {
         return mr;
     }
 
-    public void setMr(String mr) {
-        this.mr = mr;
-    }
-
-    public String getHi() {
+    String getHi() {
         return hi;
-    }
-
-    public void setHi(String hi) {
-        this.hi = hi;
     }
 
     public String getLocaleValue() {
@@ -47,7 +35,7 @@ public class LocaleData {
         } else if (Util.getLocaleLanguageCode().equalsIgnoreCase(Constants.App.LANGUAGE_HINDI)) {
             return hi;
         } else {
-            return _default;
+            return en;
         }
     }
 
@@ -57,7 +45,7 @@ public class LocaleData {
         } else if (Util.getLocaleLanguageCode().equalsIgnoreCase(Constants.App.LANGUAGE_HINDI)) {
             hi = name;
         } else {
-            _default = name;
+            en = name;
         }
     }
 }
