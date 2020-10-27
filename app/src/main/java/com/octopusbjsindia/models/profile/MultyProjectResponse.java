2bef97344a2416822bd38e3567edd1f37af49d5c
sudhirjoshibjs <sjoshi@bjsindia.org>
2020-10-27 12:36:04 +0530

    space

--- app/src/main/java/com/octopusbjsindia/models/profile/MultyProjectResponse.java
+++ app/src/main/java/com/octopusbjsindia/models/profile/MultyProjectResponse.java
@@ -6,6 +6,7 @@ import com.google.gson.annotations.SerializedName;
 import java.util.ArrayList;
 
 public class MultyProjectResponse {
+
     @SerializedName("status")
     @Expose
     private String status;
