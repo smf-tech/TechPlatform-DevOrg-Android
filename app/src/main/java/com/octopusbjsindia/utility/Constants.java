acdf8905e4554d2e461b1ed1aa0a7202765085e2
sudhirjoshibjs <sjoshi@bjsindia.org>
2021-04-24 14:21:05 +0530

    smart Girl tasks

--- app/src/main/java/com/octopusbjsindia/utility/Constants.java
+++ app/src/main/java/com/octopusbjsindia/utility/Constants.java
@@ -496,7 +496,6 @@ public class Constants {
 
 
 //prod
-/*
 
         public static final String MOCK_TEST_FORM = "5f33e9da9f8b7d2ba4493e93";
         public static final String PRE_TEST_FORM = "5f33e8f69f8b7d2ba4493e92";
@@ -516,10 +515,9 @@ public class Constants {
 
         public static final String BATCH_SUPPORT_DOC_FORM = "5faa228906398b0e09183e44";
         public static final String WORKSHOP_SUPPORT_DOC_FORM = "5faa222e244ba177f315802a";
-*/
 
 //stage
-        public static final String MOCK_TEST_FORM = "5ea82d7b2c81b43be40b21f7";
+     /*   public static final String MOCK_TEST_FORM = "5ea82d7b2c81b43be40b21f7";
         public static final String PRE_TEST_FORM = "5ea6a6b385046668f74b7f97";
 
         public static final String ORGANISER_FEEDBACK_FORM = "5ea7cb4849c5d7695b51203f";
@@ -536,7 +534,7 @@ public class Constants {
 
         public static final String BATCH_SUPPORT_DOC_FORM = "5fa64c482d366d1445425ff3";
         public static final String WORKSHOP_SUPPORT_DOC_FORM = "5fa6468e2d366d1445425ff2";
-
+*/
 
     }
 
