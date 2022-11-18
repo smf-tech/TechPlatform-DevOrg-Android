ce9227c761dc07652f2eaf14bfb5e468b6e4bc3c
sagarmahajan19 <smahajan@mutthafoundation.org>
2022-11-18 13:02:04 +0530

    Test commit

--- app/src/main/java/com/octopusbjsindia/view/fragments/MatrimonyMeetDetailFragment.java
+++ app/src/main/java/com/octopusbjsindia/view/fragments/MatrimonyMeetDetailFragment.java
@@ -387,6 +387,7 @@ public class MatrimonyMeetDetailFragment extends Fragment implements View.OnClic
         rvMeetAnalytics.setLayoutManager(mLayoutManagerLeave);
         rvMeetAnalytics.setAdapter(meetAnalyticsAdapter);
         meetContactsListAdapter.notifyDataSetChanged();
+
     }
 
     @Override
