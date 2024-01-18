477e0d5051ff3fc1126dddcb25ebc0b484efaec7
sagarmahajan19 <smahajan@mutthafoundation.org>
2024-01-18 16:06:17 +0530

    RWB Donation flow

--- app/src/main/java/com/octopusbjsindia/presenter/RWBDonationRecordPresenter.kt
+++ app/src/main/java/com/octopusbjsindia/presenter/RWBDonationRecordPresenter.kt
@@ -1,14 +1,47 @@
 package com.octopusbjsindia.presenter
 
+import android.util.Log
 import com.android.volley.VolleyError
+import com.google.gson.GsonBuilder
+import com.octopusbjsindia.BuildConfig
 import com.octopusbjsindia.listeners.APIPresenterListener
+import com.octopusbjsindia.models.SujalamSuphalam.RWBDonorApiResponse
+import com.octopusbjsindia.request.APIRequestCall
+import com.octopusbjsindia.utility.PlatformGson
+import com.octopusbjsindia.utility.Urls
 import com.octopusbjsindia.view.activities.DonationRecordActivity
 import java.lang.ref.WeakReference
 
 class RWBDonationRecordPresenter(activity: DonationRecordActivity): APIPresenterListener {
 
     private var activityWeakReference: WeakReference<DonationRecordActivity>? = null
-    private val TAG = RWBDonationRecordPresenter::class.java
+    private val TAG = RWBDonationRecordPresenter::class.java.name
+
+    companion object {
+        const val GET_DISTRICT_DONOR = "getDistrictDonors"
+    }
+
+    init {
+        activityWeakReference = WeakReference(activity)
+    }
+
+    fun getDistrictDonorsList(districtId: String, structureId: String) {
+        val gson = GsonBuilder().create()
+        activityWeakReference!!.get()?.showProgressBar()
+        val map = HashMap<String, String>()
+        map["district_id"] = districtId
+        map["structure_id"] = structureId
+        val paramJson = gson.toJson(map)
+        val getDonorListUrl = (BuildConfig.BASE_URL + String.format(Urls.SSModule.GET_RWB_DONORS))
+        Log.d(TAG, "donor list url: $getDonorListUrl")
+        val requestCall = APIRequestCall()
+        requestCall.setApiPresenterListener(this)
+        requestCall.postDataApiCall(
+            RWBDonationRecordPresenter.GET_DISTRICT_DONOR,
+            paramJson,
+            getDonorListUrl
+        )
+    }
 
     override fun onFailureListener(requestID: String?, message: String?) {
         TODO("Not yet implemented")
@@ -19,8 +52,34 @@ class RWBDonationRecordPresenter(activity: DonationRecordActivity): APIPresenter
     }
 
     override fun onSuccessListener(requestID: String?, response: String?) {
-        TODO("Not yet implemented")
+        if (activityWeakReference == null) {
+            return
+        }
+        activityWeakReference?.get()?.hideProgressBar()
+
+        try {
+            if (response != null) {
+                if (requestID.equals(GET_DISTRICT_DONOR, ignoreCase = true)) {
+                    val districtDonorListData = PlatformGson.getPlatformGsonInstance().fromJson(
+                        response,
+                        RWBDonorApiResponse::class.java
+                    )
+                    if (districtDonorListData.code == 200) {
+                        activityWeakReference?.get()
+                            ?.populateProspectDonorList(requestID, districtDonorListData)
+                    } else if (districtDonorListData.code == 400) {
+                        activityWeakReference?.get()?.showNoDataMessage()
+                    }
+
+                }
+            }
+        } catch (e: Exception) {
+            activityWeakReference?.get()?.onFailureListener(requestID, e.message)
+        }
+
     }
 
-
+    fun clearData() {
+        activityWeakReference = null
+    }
 }
--- app/src/main/java/com/octopusbjsindia/view/activities/DonationRecordActivity.kt
+++ app/src/main/java/com/octopusbjsindia/view/activities/DonationRecordActivity.kt
@@ -1,17 +1,24 @@
 package com.octopusbjsindia.view.activities
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.android.volley.VolleyError
 import com.octopusbjsindia.databinding.ActivityDonationRecordBinding
+import com.octopusbjsindia.listeners.APIDataListener
+import com.octopusbjsindia.listeners.CustomSpinnerListener
+import com.octopusbjsindia.models.SujalamSuphalam.RWBDonorApiResponse
 import com.octopusbjsindia.presenter.RWBDonationRecordPresenter
+import com.octopusbjsindia.utility.Util
 
-class DonationRecordActivity: AppCompatActivity() {
+class DonationRecordActivity: AppCompatActivity(), APIDataListener, CustomSpinnerListener {
 
     private var _binding: ActivityDonationRecordBinding? = null
     private val binding get() = _binding!!
-
     private var presenter: RWBDonationRecordPresenter? = null
+    private var structureDistrictId: String? = null
+    private var structureDistrict: String? = null
+    private var structureId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +28,74 @@ class DonationRecordActivity: AppCompatActivity() {
         binding.toolbar.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
-        presenter = RWBDonationRecordPresenter(this)
 
+        structureDistrictId = intent.getStringExtra("districtId")
+        structureDistrict = intent.getStringExtra("district")
+        structureId = intent.getStringExtra("structureId")
+
+        presenter = RWBDonationRecordPresenter(this)
+        presenter?.getDistrictDonorsList(structureDistrictId!!, structureId!!)
+
+    }
+
+    fun populateDistrictDonorList(requestID: String?, data: RWBDonorApiResponse?) {
+        if (data != null) {
+
+        } else {
+            showNoDataMessage()
+        }
+    }
+
+    fun showNoDataMessage() {
+        Util.showToast("No donor available for your/selected district.", this)
+    }
+
+    override fun onFailureListener(requestID: String?, message: String?) {
+        Util.showToast(message.toString(), this)
+    }
+
+    override fun onErrorListener(requestID: String?, error: VolleyError?) {
+        Util.showToast(error?.message.toString(), this)
+    }
+
+    override fun onSuccessListener(requestID: String?, response: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showProgressBar() {
+        runOnUiThread {
+            binding?.apply {
+                lytProgressBar?.apply {
+                    isVisible = true
+                }
+            }
+        }
+    }
+
+    override fun hideProgressBar() {
+        runOnUiThread {
+            binding?.apply {
+                lytProgressBar?.apply {
+                    isVisible = false
+                }
+            }
+        }
+    }
+
+    override fun closeCurrentActivity() {
+        finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (presenter != null) {
+            presenter?.clearData()
+            presenter = null
+        }
+    }
+
+    override fun onCustomSpinnerSelection(type: String?) {
+        TODO("Not yet implemented")
     }
 
 }
