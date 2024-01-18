cbefa5b3f4f249f8479b6caa3282913c649a3e11
AkshayBJS <anighot@bjsindia.org>
2024-01-18 16:04:32 +0530

    donation record UI implemented

--- app/src/main/java/com/octopusbjsindia/view/activities/DonationRecordActivity.kt
+++ app/src/main/java/com/octopusbjsindia/view/activities/DonationRecordActivity.kt
@@ -4,25 +4,27 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.octopusbjsindia.databinding.ActivityDonationRecordBinding
+import com.octopusbjsindia.databinding.ActivityDonorsBinding
 import com.octopusbjsindia.presenter.RWBDonationRecordPresenter
 
 class DonationRecordActivity: AppCompatActivity() {
-
-    private var _binding: ActivityDonationRecordBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: ActivityDonationRecordBinding
 
     private var presenter: RWBDonationRecordPresenter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityDonationRecordBinding.inflate(layoutInflater)
+        binding = ActivityDonationRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.toolbar.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
         presenter = RWBDonationRecordPresenter(this)
 
     }
+
+
 
 }
